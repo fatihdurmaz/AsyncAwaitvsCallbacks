7e5ea10dcf898a4b3f53aa0f5ade425fe202dbb5
Fatih DURMAZ <fatih.durmaz@outlook.com>
2024-07-23 14:24:14 +0300

    Initial Commit

---
+++ AsyncAwaitvsCallbacks/AsyncAwaitvsCallbacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitvsCallbacksApp.swift
+//  AsyncAwaitvsCallbacks
+//
+//  Created by Fatih Durmaz on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitvsCallbacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncAwaitvsCallbacks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsyncAwaitvsCallbacks
+//
+//  Created by Fatih Durmaz on 23.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
